176238a19c4104ff0bf8e40e04639143931608b1
Nomad192 <verenya08@gmail.com>
2023-06-24 19:42:24 +0300

    T1 optim

--- Sources/helper/package_dataset/__init__.py
+++ Sources/helper/package_dataset/__init__.py
@@ -1,3 +1,4 @@
 from .module_generator import *
 from .module_save_load import *
 from .module_print import *
+from .module_func import *
---
+++ Sources/helper/package_dataset/module_func.py
@@ -0,0 +1,8 @@
+import types
+
+def get_func_research(f_label):
+    compiled_function = compile(f_label, "<string>", "exec")
+    exec(compiled_function)
+
+    # Создаем объект функции из скомпилированного кода
+    return types.FunctionType(compiled_function.co_consts[0], globals())
